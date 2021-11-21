832299ff4b4bc339767423ffbd2c8ebc60f0c790
Geovane Araújo <geovane-araujo@outlook.com>
2021-11-20 21:48:59 -0300

    starter

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+*{
+  margin: 0px;
+  padding: 0px;
+}
+body{
+  height: 100vh;
+  width: 100vw;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/componentes/menuone/KonosubaUi.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main">
+    Olá Mundo
+  </div>
+</template>
+
+<script src="./KonosubaUi.js">
+</script>
+
+<style lang="scss" scoped>
+@import './KonosubaUi.scss';
+</style>
